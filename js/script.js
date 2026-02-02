0985e342e15f1eeb228aaef8106eeed924c7fdb8
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-02-02 08:27:00 +0100

    Added drawing on canvas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const canvas = document.getElementById('canvas');
 
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+const ctx = canvas.getContext('2d');
+let lastPos = null;
+
 function mouse_pos(event) {
     const rect = canvas.getBoundingClientRect();
     return {
@@ -10,6 +16,25 @@ function mouse_pos(event) {
 
 canvas.onmousemove = function (event) {
     let pos = mouse_pos(event);
-    console.log(pos);
+    drawOnCanvas(pos.x, pos.y, 'black', ctx);
+}
 
+canvas.onmousedown = function (event) {
+    let pos = mouse_pos(event);
+    lastPos = pos;
+}
+
+function drawOnCanvas(x, y, color, ctx) {
+    if (lastPos === null) return; // Return at the start when lastPost = null
+    
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.lineCap = 'round';
+    
+    ctx.beginPath();
+    ctx.moveTo(lastPos.x, lastPos.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    
+    lastPos = { x: x, y: y };
 }
