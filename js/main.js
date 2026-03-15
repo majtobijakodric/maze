b78101ed7dcca899f730546e9416bb9ba5008621
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-03-15 15:22:56 +0100

    Changed maze size

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const howToPlay = document.getElementById('how-to-play-btn');
 const aboutBtn = document.getElementById('about-btn');
 
 // Random maze size (must be odd)
-const width = 101;
-const height = 101;
+const width = 25;
+const height = 25;
 
 const open = 1;
 const closed = 0;
